431033ccb0e11f619965b2e9d365fec49e15136a
Karl Hobley <karlhobley10@gmail.com>
2013-05-26 16:47:22 +0100

    Account closure working

--- includes/account/close_account.php
+++ includes/account/close_account.php
@@ -1,6 +1,35 @@
-<p>You account cannot be reopened once it is closed</p>
+<?php
+    // Success flag
+    $success = false;
+    
+    // Check if were closing the account
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get confirmation
+        $confirm = $_POST["confirm"];
+        
+        // If confirm is true, close the account
+        if ($confirm == "true") {
+            // Close account
+            auth_close_account($db, $user["id"]);
+            
+            // Logout of session
+            auth_logout($db, $_SESSION["session_id"]);
+            $_SESSION["session_id"] = null;
+            
+            // Set success flag
+            $success = true;
+        }
+    }
+?>
 
-<form action="account.php?action=close_account" method="post">
-    <input type="hidden" name="confirm" value="true" />
-    <input type="submit" value="Close account" />
-</form>
+<?php if($success) { ?>
+    <h3>Account closed</h3>
+    <p><a href="index.php">Go back to home page</a></p>
+<?php } else { ?>
+    <p>You account cannot be reopened once it is closed</p>
+
+    <form action="account.php?action=close_account" method="post">
+        <input type="hidden" name="confirm" value="true" />
+        <input type="submit" value="Close account" />
+    </form>
+<?php } ?>
