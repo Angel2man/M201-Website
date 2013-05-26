4bbfdcd99d01160f66a12eb0a2376b4866733e6e
Karl Hobley <karlhobley10@gmail.com>
2013-05-26 18:07:12 +0100

    Started adding some sample data

--- load_sample_data.php
+++ load_sample_data.php
@@ -1,12 +1,47 @@
 <?php
+    function add_product($db, $name, $summary, $description, $category, $usual_price, $price) {
+        
+    }
+    
+    function add_category($db, $name, $position) {
+        
+    }
+    
     function load_sample_data($db) {
-        // Load 100 products
-        for ($i = 0; $i < 100; $i++) {
-            $product_name = "Product ".($i + 1);
-            
-            $db->query("INSERT INTO product (name, summary, description, price, usual_price, stock) VALUES
-                        (\"$product_name\", \"Summary\", \"Description\", 500, 1000, 0)");
-        }
+        // Categories
+        $book_category = add_category($db, "Books", 0);
+        $dvd_category = add_category($db, "DVDs", 1);
+        $game_category = add_category($db, "Games", 2);
+        $music_category = add_category($db, "Music", 3);
+        
+        // Books
+        add_product($db, "OpenGL Programming Guide",
+                         "A book about programming with the OpenGL graphics API",
+                         "A book about programming with the OpenGL graphics API",
+                         $book_category, 2000, 2000);
+                         
+        add_product($db, "Beginning databases with PostgreSQL",
+                         "Matthew, Stones. A book about database development using the PostgreSQL database management system",
+                         "A book about database development using the PostgreSQL database management system",
+                         $book_category, 1000, 1000);
+                         
+        add_product($db, "Effective C++",
+                         "Scott Meyers. A book for C++ developers to help them improve their skills",
+                         "A book for C++ developers to help them improve their skills",
+                         $book_category, 500, 500);
+                         
+        add_product($db, "The Cathedral and the Bazaar",
+                         "Eric S. Raymond. Musings on Linux and open source by an accidental revolutionary",
+                         "Musings on Linux and open source by an accidental revolutionary",
+                         $book_category, 500, 500);
+                         
+        add_product($db, "Javascript: The Definitive Guide",
+                         "David Flanagan. Enormous book about javascript",
+                         "Enormous book about javascript",
+                         $book_category, 2000, 1500);
+                         
+        // DVDs
+        
     }
     
     
