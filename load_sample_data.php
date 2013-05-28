fa6863597c046d9f9a8afbb055dc3dc382577ba0
Karl Hobley <karlhobley10@gmail.com>
2013-05-28 14:08:36 +0100

    Improved sample data

--- load_sample_data.php
+++ load_sample_data.php
@@ -1,10 +1,17 @@
 <?php
-    function add_product($db, $name, $summary, $description, $category, $usual_price, $price) {
-        
-    }
+    function add_product($db, $name, $summary, $description, $category_id, $usual_price, $price) {
+        $db->query("INSERT INTO product (name, summary, description, category_id, usual_price, price) VALUES (\"$name\", \"$summary\", \"$description\", $category_id, $usual_price, $price)");
+	}
     
     function add_category($db, $name, $position) {
-        
+        // Create a new category
+		if($db->query("INSERT INTO category (name, position) VALUES (\"$name\", $position)")) {
+			// Return new category ID
+			return $db->insert_id;
+		} else {
+			// Return null
+			return null;
+		}
     }
     
     function load_sample_data($db) {
@@ -23,7 +30,7 @@
         add_product($db, "Beginning databases with PostgreSQL",
                          "Matthew, Stones. A book about database development using the PostgreSQL database management system",
                          "A book about database development using the PostgreSQL database management system",
-                         $book_category, 1000, 1000);
+                         $book_category, 1000, 750);
                          
         add_product($db, "Effective C++",
                          "Scott Meyers. A book for C++ developers to help them improve their skills",
@@ -36,12 +43,61 @@
                          $book_category, 500, 500);
                          
         add_product($db, "Javascript: The Definitive Guide",
-                         "David Flanagan. Enormous book about javascript",
-                         "Enormous book about javascript",
+                         "David Flanagan. Enormous book about Javascript",
+                         "Enormous book about Javascript",
                          $book_category, 2000, 1500);
                          
         // DVDs
-        
+        add_product($db, "Lord of the Rings: The Two Towers",
+                         "",
+                         "",
+                         $dvd_category, 2000, 1500);
+                         
+        add_product($db, "The Shawshank Redemption",
+                         "",
+                         "",
+                         $dvd_category, 2000, 1000);
+                         
+        add_product($db, "The Bourne Identity",
+                         "",
+                         "",
+                         $dvd_category, 2000, 2000);
+                         
+        add_product($db, "Marley",
+                         "A documentary about Bob Marley",
+                         "A documentary about Bob Marley",
+                         $dvd_category, 2000, 2000);
+                         
+        add_product($db, "Family Guy Season 3",
+                         "",
+                         "",
+                         $dvd_category, 2000, 2000);
+                         
+        // Games
+        add_product($db, "Battlefield 3",
+                         "PC/DVD (Windows)",
+                         "",
+                         $game_category, 3000, 3000);
+                         
+        add_product($db, "Halo 3",
+                         "Xbox 360",
+                         "",
+                         $game_category, 3000, 3000);
+                         
+        add_product($db, "ArmA: Armed Assault",
+                         "PC/DVD (Windows)",
+                         "",
+                         $game_category, 2500, 2500);
+                         
+        add_product($db, "Far Cry 3",
+                         "Xbox 360",
+                         "",
+                         $game_category, 4000, 4000);
+                         
+        add_product($db, "Halo: Combat Evolved",
+                         "Xbox",
+                         "",
+                         $game_category, 4000, 4000);
     }
     
     
