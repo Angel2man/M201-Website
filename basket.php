905af9b2f43e7bf566833bdd6d03ec04943b8d1f
Karl Hobley <karlhobley10@gmail.com>
2013-06-04 17:40:58 +0100

    Code cleanup

--- basket.php
+++ basket.php
@@ -8,6 +8,15 @@
         forward("login.php?next=basket.php", null);
     }
     
+    // Check for users basket if were not changing it
+    if (!$user["basket"] && $_GET["action"] != "change_item") {
+        // Print error
+        require "includes/header.php";
+        echo "<p>Your basket is empty</p>";
+        require "includes/footer.php";
+        die();
+    }
+    
     // Set page variables
     $page["title"] = "Basket";
     
@@ -18,58 +27,46 @@
         
         // Print change item page
         require "includes/basket/change_item.php";
-    } else {
-        // Check for users basket
-        if (!$user["basket"]) {
-            // Print error
-            require "includes/header.php";
-            echo "<p>Your basket is empty</p>";
-            require "includes/footer.php";
-            die();
-        }
+    } else if ($_GET["action"] == "checkout") { // Check if were checking out
+        // Change title to checkout
+        $page["title"] = "Checkout";
         
-        // Check if were checking out
-        if ($_GET["action"] == "checkout") {
-            // Change title to checkout
-            $page["title"] = "Checkout";
+        // Add some breadcrumb
+        $page["breadcrumb"] = array(
+            array("Basket", "basket.php"),
+            array("Checkout", null)
+        );
+        
+        // Header
+        require "includes/header.php";
+        
+        // If were posting, run do_checkout
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            require "includes/basket/do_checkout.php";
             
-            // Add some breadcrumb
-            $page["breadcrumb"] = array(
-                array("Basket", "basket.php"),
-                array("Checkout", null)
-            );
-            
-            // Header
-            require "includes/header.php";
-            
-            // If were posting, run do_checkout
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                require "includes/basket/do_checkout.php";
-                
-                // If theres a validation error, print out checkout page
-                if ($address_invalid) {
-                    require "includes/basket/checkout.php";
-                }
-            } else {
-                // Get address
-                $name = $user["name"];
-                $address1 = $user["address1"];
-                $address2 = $user["address2"];
-                $town = $user["town"];
-                $county = $user["county"];
-                $postcode = $user["postcode"];
-                $phone = $user["phone"];
-                
-                // Print checkout page
+            // If theres a validation error, print out checkout page
+            if ($address_invalid) {
                 require "includes/basket/checkout.php";
             }
-        } else { // Print basket
-            // Header
-            require "includes/header.php";
+        } else {
+            // Get address
+            $name = $user["name"];
+            $address1 = $user["address1"];
+            $address2 = $user["address2"];
+            $town = $user["town"];
+            $county = $user["county"];
+            $postcode = $user["postcode"];
+            $phone = $user["phone"];
             
-            // Print basket page
-            require "includes/basket/basket.php";
+            // Print checkout page
+            require "includes/basket/checkout.php";
         }
+    } else { // Print basket
+        // Header
+        require "includes/header.php";
+        
+        // Print basket page
+        require "includes/basket/basket.php";
     }
     
     // Footer
